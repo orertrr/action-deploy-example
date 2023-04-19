14d308caadae08992ef1d557fdc6ef5bbc314b8f
orertrr <orertrr6207@gmail.com>
2023-04-20 00:01:17 +0800

    Add  argument for gpg
    Signed-off-by: orertrr <orertrr6207@gmail.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -10,7 +10,7 @@ sudo apt install -y \
     gnupg
 
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --no-tty --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
